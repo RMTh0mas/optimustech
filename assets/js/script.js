0834f9290d4643350f89158569fb3e09a110568f
RMTh0mas <renanmiguelthomas@gmail.com>
2025-02-23 01:02:27 -0300

    Ajuste de rotas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("/vagas.html")
+    fetch("../../vagas.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("vagas").innerHTML = data;
     })
     .catch(error => console.error("Erro ao carregar a página Sobre:", error));
 
-    fetch("/feedbacks.html")
+    fetch("../../feedbacks.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("feedbacks").innerHTML = data;
     })
     .catch(error => console.error("Erro ao carregar a página Sobre:", error));
 
-    fetch("/rodape.html")
+    fetch("../../rodape.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("rodape").innerHTML = data;
